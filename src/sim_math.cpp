5a429906acb7bc7443dc6dfcb1839657b6959b67
KozhaAkhmet <kozha.akhmt@gmail.com>
2026-04-11 22:20:47 +0300

    Corrected seperation algorithm

--- src/sim_math.cpp
+++ src/sim_math.cpp
@@ -41,6 +41,7 @@ void SimMath::separation(std::shared_ptr<Fish> fish, const std::vector<std::shar
     for (auto& n : fishes_nearby) {
         sum += fish->getPosition() - n->getPosition();
     }
+    std::cout << sum.x << " " << sum.y << std::endl;
     fish->setSepVec(sum);
 }
 
@@ -70,32 +71,32 @@ void SimMath::cohesion(std::shared_ptr<Fish> fish, const std::vector<std::shared
 void SimMath::applyModifiedDirection(std::shared_ptr<Fish> fish) {
     float distance_divider = fish->getMinDistance();
 
-    sf::Vector2f coh_vec = fish->getCohVec();
     sf::Vector2f fish_pos = fish->getPosition();
     float dir = fish->getDirection();
     
     const float sep_const = 0.004, allign_const = 0.007, coh_const = 0.006;
     //------------Seperation----------
-    sf::Vector2f sum_vec = fish->getSepVec();
-    float sep_ang;
-    if (sum_vec.x != 0 && sum_vec.y != 0) {
-        float evade_ang, sep_rel_ang, positive_sep_ang,
-        positive_sep_rel_ang;
-        sep_ang = -(atan2(sum_vec.x, sum_vec.y));
-        positive_sep_ang = sep_ang < 0 ? sep_ang + PI_M_2 : sep_ang;
+    sf::Vector2f sep_vec = fish->getSepVec();
+    float sep_local_ang;
+    if (sep_vec.x != 0 && sep_vec.y != 0) {
+        float evade_ang, sep_local_dir_ang, sep_ang_relative_neg_y, positive_sep_local_ang;
+
+        //The sep_vec is the sum of fish directions.
+        //For fish, 0 angle is neg y and sep_ang_relative_neg_y become reletive to negative y.
+        sep_ang_relative_neg_y = atan2f(sep_vec.x,sep_vec.y);
         
-        sep_rel_ang = positive_sep_ang - dir + PI_D_2;
-        positive_sep_rel_ang = sep_rel_ang < 0 ? sep_rel_ang + PI_M_2 : sep_rel_ang;
-        
-        evade_ang = sep_ang + PI_D_2;
-        if (positive_sep_rel_ang < PI) dir += sep_const * abs(sep_rel_ang);
-        if (positive_sep_rel_ang > PI) dir -= sep_const * abs(sep_rel_ang);
+        //the sep_ang_relative_neg_y and dir are already directional (they can be negative).
+        sep_local_ang = dir - sep_ang_relative_neg_y;
+
+        sep_local_ang += PI; // The ang should be at opposite side
+
+        dir += sep_const * sep_local_ang;
     }
 
     sf::VertexArray sep_lines(sf::PrimitiveType::LineStrip, 2);
     sep_lines[0].position = fish_pos;
     sep_lines[1].position =
-        SimMath::polarToCortesian(sep_ang + PI_D_2) * fish.get()->getCollisionRadius() + fish_pos;
+        SimMath::polarToCortesian(sep_local_ang) * fish.get()->getCollisionRadius() + fish_pos;
     sep_lines[1].color = sf::Color::Red;
 
     //------------Alignment----------
@@ -112,20 +113,23 @@ void SimMath::applyModifiedDirection(std::shared_ptr<Fish> fish) {
     align_lines[1].color = sf::Color::Green;
 
     //------------Cohesion----------
+    sf::Vector2f coh_vec = fish->getCohVec();
     float coh_rad, positive_coh_ang, coh_rel_ang, positive_coh_rel_ang;
-    coh_rad = -(atan2(coh_vec.x, coh_vec.y));
-    positive_coh_ang = coh_rad < 0 ? coh_rad + PI_M_2 : coh_rad;
+    if(coh_vec.x != 0 && coh_vec.y !=0){
+        coh_rad = -(atan2(coh_vec.x, coh_vec.y));
+        positive_coh_ang = coh_rad < 0 ? coh_rad + PI_M_2 : coh_rad;
 
-    coh_rel_ang = positive_coh_ang - dir + PI_D_2;
-    positive_coh_rel_ang = coh_rel_ang < 0 ? coh_rel_ang + PI_M_2 : coh_rel_ang;
-    if (positive_coh_rel_ang < PI) dir -= coh_const * abs(coh_rel_ang);
-    if (positive_coh_rel_ang > PI) dir += coh_const * abs(coh_rel_ang);
-
+        coh_rel_ang = positive_coh_ang - dir + PI_D_2;
+        positive_coh_rel_ang = coh_rel_ang < 0 ? coh_rel_ang + PI_M_2 : coh_rel_ang;
+        if (positive_coh_rel_ang < PI) dir -= coh_const * abs(coh_rel_ang);
+        if (positive_coh_rel_ang > PI) dir += coh_const * abs(coh_rel_ang);
+    }
     sf::VertexArray coh_lines(sf::PrimitiveType::LineStrip, 2);
     coh_lines[0].position = fish_pos;
     coh_lines[1].position =
-        SimMath::polarToCortesian(allign_ang_rad + PI_D_2) * fish.get()->getCollisionRadius() + fish_pos;
+        SimMath::polarToCortesian(coh_rad + PI_D_2) * fish.get()->getCollisionRadius() + fish_pos;
     coh_lines[1].color = sf::Color::Yellow;
+    
     //------------------------------    
     
     if (dir < 0) dir += PI_M_2;
